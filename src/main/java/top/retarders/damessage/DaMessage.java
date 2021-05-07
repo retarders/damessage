06b45722c4eba684f81b4daf886b09c46d5b796f
pcranaway <pcranaway@tuta.io>
2021-05-07 16:38:28 +0300

    remove caching and use setDescription because components are confusing

--- src/main/java/top/retarders/damessage/DaMessage.java
+++ src/main/java/top/retarders/damessage/DaMessage.java
@@ -1,26 +1,15 @@
 package top.retarders.damessage;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
-import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ServerPing;
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
-import net.md_5.bungee.config.Configuration;
-import net.md_5.bungee.config.ConfigurationProvider;
-import net.md_5.bungee.config.YamlConfiguration;
 import net.md_5.bungee.event.EventHandler;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 public class DaMessage extends Plugin {
-    private ServerPing serverPing;
 
     // clang-format off
     private static String[] messages = new String[] {
@@ -35,25 +24,12 @@ public class DaMessage extends Plugin {
 
     @Override
     public void onEnable() {
-        this.getProxy().getScheduler().schedule(this, () -> {
-            String message = messages[(int) Math.floor(new Random().nextInt() * messages.length)];
-            BaseComponent[] component = new ComponentBuilder("")
-                                            .color(ChatColor.LIGHT_PURPLE)
-                                            .bold(true)
-                                            .append("Retarders Network")
-                                            .color(ChatColor.GRAY)
-                                            .append("- The Reconnaissance")
-                                            .append("\n")
-                                            .color(ChatColor.GRAY)
-                                            .append("> " + message)
-                                            .create();
-            serverPing = new ServerPing();
-            serverPing.setDescriptionComponent(component[0]);
-        }, 3000, TimeUnit.MILLISECONDS);
-
         this.getProxy().getPluginManager().registerListener(this, new Listener() {
             @EventHandler
             public void onPing(ProxyPingEvent event) {
+                String message = messages[(int) Math.floor(new Random().nextInt() * messages.length)];
+                ServerPing serverPing = new ServerPing();
+                serverPing.setDescription("&d&lRetarders Network &7- The Reconnaissance\n&7> " + message);
                 event.setResponse(serverPing);
             }
         });
