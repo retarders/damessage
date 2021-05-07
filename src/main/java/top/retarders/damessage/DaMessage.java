061bccde3f513dd718c74da9fa2a3b419bd62f54
pcranaway <pcranaway@tuta.io>
2021-05-07 16:39:15 +0300

    format

--- src/main/java/top/retarders/damessage/DaMessage.java
+++ src/main/java/top/retarders/damessage/DaMessage.java
@@ -1,16 +1,14 @@
 package top.retarders.damessage;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
 
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
-
 public class DaMessage extends Plugin {
-
     // clang-format off
     private static String[] messages = new String[] {
         "retarders > all",
@@ -27,9 +25,11 @@ public class DaMessage extends Plugin {
         this.getProxy().getPluginManager().registerListener(this, new Listener() {
             @EventHandler
             public void onPing(ProxyPingEvent event) {
-                String message = messages[(int) Math.floor(new Random().nextInt() * messages.length)];
+                String message =
+                    messages[(int) Math.floor(new Random().nextInt() * messages.length)];
                 ServerPing serverPing = new ServerPing();
-                serverPing.setDescription("&d&lRetarders Network &7- The Reconnaissance\n&7> " + message);
+                serverPing.setDescription(
+                    "&d&lRetarders Network &7- The Reconnaissance\n&7> " + message);
                 event.setResponse(serverPing);
             }
         });
