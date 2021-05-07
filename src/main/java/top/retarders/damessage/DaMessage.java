6229d9589b38e1e8611d3c12120941482bf1a18a
pcranaway <pcranaway@tuta.io>
2021-05-07 15:34:48 +0300

    lessgo

---
+++ src/main/java/top/retarders/damessage/DaMessage.java
@@ -0,0 +1,15 @@
+package top.retarders.damessage;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class DaMessage extends Plugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+}
