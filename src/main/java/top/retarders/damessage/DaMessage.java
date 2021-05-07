93be3f8a1e9766982dd454e277ae685cfb631614
pcranaway <pcranaway@tuta.io>
2021-05-07 16:42:09 +0300

    move listener to the class

--- src/main/java/top/retarders/damessage/DaMessage.java
+++ src/main/java/top/retarders/damessage/DaMessage.java
@@ -8,7 +8,7 @@ import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
 
-public class DaMessage extends Plugin {
+public class DaMessage extends Plugin implements Listener {
     // clang-format off
     private static String[] messages = new String[] {
         "retarders > all",
@@ -22,16 +22,16 @@ public class DaMessage extends Plugin {
 
     @Override
     public void onEnable() {
-        this.getProxy().getPluginManager().registerListener(this, new Listener() {
-            @EventHandler
-            public void onPing(ProxyPingEvent event) {
-                String message =
-                    messages[(int) Math.floor(new Random().nextInt() * messages.length)];
-                ServerPing serverPing = new ServerPing();
-                serverPing.setDescription(
-                    "&d&lRetarders Network &7- The Reconnaissance\n&7> " + message);
-                event.setResponse(serverPing);
-            }
-        });
+        this.getProxy().getPluginManager().registerListener(this, this);
     }
+    @EventHandler
+    public void onPing(ProxyPingEvent event) {
+//                String message =
+//                    messages[(int) Math.floor(new Random().nextInt() * messages.length)];
+        ServerPing serverPing = new ServerPing();
+        serverPing.setDescription(
+                "&d&lRetarders Network &7- The Reconnaissance\n&7> ");
+        event.setResponse(serverPing);
+    }
+
 }
