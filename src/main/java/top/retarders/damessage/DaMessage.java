ca1b645527dbc9af1ac38d22e7fe61db9c5fd5d7
pcranaway <pcranaway@tuta.io>
2021-05-07 16:44:48 +0300

    translate color codes

--- src/main/java/top/retarders/damessage/DaMessage.java
+++ src/main/java/top/retarders/damessage/DaMessage.java
@@ -2,6 +2,8 @@ package top.retarders.damessage;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
+
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.plugin.Listener;
@@ -28,7 +30,8 @@ public class DaMessage extends Plugin implements Listener {
     public void onPing(ProxyPingEvent event) {
         String message = messages[(int) Math.floor(new Random().nextInt() * messages.length)];
         ServerPing serverPing = new ServerPing();
-        serverPing.setDescription("&d&lRetarders Network &7- The Reconnaissance\n&7> " + message);
+        serverPing.setDescription(ChatColor.translateAlternateColorCodes(
+            '&', "&d&lRetarders Network &7- The Reconnaissance\n&7> " + message));
         event.setResponse(serverPing);
     }
 }
